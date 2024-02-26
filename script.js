64eb6e3d5f4b67de0534ddbcf8008223eb4d6329
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-02-27 01:29:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert(";(")
+Introduction = "Hey! I am Shayan Babar"
+Hobby = "Programmer"
+Age = "I am 18th years old"
+Qualification = "Intermediate"
+Field = "Computer Science"
+console.log(Introduction)
+console.log(Hobby)
+console.log(Age)
+console.log(Qualification)
+console.log(Field)
